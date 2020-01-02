2d785734964160bdfc9398eefdc0bc6f91e1c0f6
kaiomagalhaes <me@kaiomagalhaes.com>
2020-01-02 16:37:08 -0300

    update post request to read type json

--- index.js
+++ index.js
@@ -5,20 +5,18 @@ const express = require('express')
 const Parsers = require('./Parsers')
 
 const app = express()
-app.use(express.urlencoded({
-  extended: false
-}));
+app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3000
 
 const ZAPIER_WEBHOOK_URL = process.env.ZAPIER_WEBHOOK_URL;
 
 app.post('/', (req, res) => {
-  const { payload } = req.body;
-  const json = JSON.parse(payload)
-
+  const json = req.body;
+  console.log(json)
   const Parser = Parsers.getParser(json)
   const content = Parser.getContent(json)
+  console.log(content)
 
   axios.post(ZAPIER_WEBHOOK_URL, content)
 
