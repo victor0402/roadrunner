eeb5541bb6dc2a56dea42083ef2ab4de27ef7d1f
kaiomagalhaes <me@kaiomagalhaes.com>
2020-01-02 22:38:21 -0300

    ignore draft prs

--- Parsers/index.js
+++ Parsers/index.js
@@ -2,7 +2,7 @@ const NewPRParser = require('./NewPRParser');
 const NewCommentParser = require('./NewCommentParser');
 
 const getParser = (json) => {
-  if (json.action === 'opened') {
+  if (json.action === 'opened' && !json.draft) {
     return NewPRParser;
   } else if(json.action === 'created' && json.comment && json.comment.url) {
     return NewCommentParser;
