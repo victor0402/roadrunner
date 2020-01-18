608c7a049a265590f2292f204ba4f2ceece88129
kaiomagalhaes <me@kaiomagalhaes.com>
2020-01-17 23:24:40 -0300

    clean db.mjs

--- db.mjs
+++ db.mjs
@@ -1,58 +1,18 @@
 import mongo from 'mongodb';
 
+const DATABASE_NAME = 'keys';
+
 class Database {
-  async getClient() {
+  static async getCollection(collectionName) {
     const url = process.env.MONGO_URL;
     const client = await mongo.connect(url, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
 
-    client.db('keys');
-
-    return client.db('keys');
-  }
-
-  async getCollection(collectionName) {
-    const url = process.env.MONGO_URL;
-    const client = await mongo.connect(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
-
-    const database = client.db('keys');
+    const database = client.db(DATABASE_NAME);
 
     return database.collection(collectionName)
-  }
-
-  async save(key, value) {
-    const client = await getClient();
-    const db = client.db('keys')
-
-    const collection = db.collection('keys')
-
-    collection.insertOne({
-      key,
-      value
-    })
-  }
-
-  async retrieve(key) {
-    const client = await getClient();
-    const db = client.db('keys')
-
-    const collection = db.collection('keys')
-    const result = await collection.findOne({ key })
-    return result.value
-  }
-
-  async destroy(key) {
-    const client = await getClient();
-    const db = client.db('keys')
-
-    const collection = db.collection('keys')
-    const result = await collection.deleteOne({ key })
-    return result.value
   }
 }
 
