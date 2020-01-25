a7049cdf9f87361e3bfa86c9a14beebda5e28b01
kaiomagalhaes <me@kaiomagalhaes.com>
2020-01-25 11:24:49 -0300

    refactor Database.js to become a singleton

--- src/services/Database.js
+++ src/services/Database.js
@@ -3,12 +3,27 @@ import mongo from 'mongodb';
 const DATABASE_NAME = 'keys';
 
 class Database {
-  static async getCollection(collectionName) {
-    const url = process.env.MONGO_URL;
-    const client = await mongo.connect(url, {
+  constructor() {
+    this.mongoUrl = process.env.MONGO_URL;
+  }
+
+  async setClient() {
+    this.client = await mongo.connect(this.mongoUrl, {
       useNewUrlParser: true,
       useUnifiedTopology: true
-    })
+    });
+  }
+
+  static async getInstance() {
+    if (!Database.instance) {
+      Database.instance = new Database();
+      await Database.instance.setClient();
+    }
+    return Database.instance;
+  }
+
+  static async getCollection(collectionName) {
+    const client = (await Database.getInstance()).client;
 
     const database = client.db(DATABASE_NAME);
 
