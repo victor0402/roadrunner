30454e40e8e1d048afadf8a147895440a92362a1
kaiomagalhaes <me@kaiomagalhaes.com>
2020-02-05 11:13:33 -0300

    add servers configuration

--- src/services/SlackRepository.js
+++ src/services/SlackRepository.js
@@ -11,32 +11,31 @@ const repositoriesIdentifiers = {
     channel: 'team-website-dev',
     deployChannel: 'team-website-deploy',
     owner: 'codelittinc',
-    supports_deploy: true
+    supports_deploy: true,
+    servers: ['dev-website-codelitt', 'qa-codelitt-website', 'prod-codelitt-website']
   },
   'rolli': {
     devGroup: '@rolli-devs',
     channel: 'team-rolli-dev',
     deployChannel: 'team-rolli-deploy',
     owner: 'codelittinc',
-    supports_deploy: true
+    supports_deploy: true,
+    servers: ['dev-rolli', 'qa-rolli', 'prod-rolli']
   },
   'team-maker': {
     devGroup: '@team-maker-devs',
     channel: 'team-teammaker-dev',
     deployChannel: 'wg-teammaker-deploy',
     owner: 'codelittinc',
-    supports_deploy: true
+    supports_deploy: true,
+    servers: ['dev-team-maker', 'qa-team-maker', 'prod-team-maker']
   },
   'zonda': {
     devGroup: '@zonda-devs',
     channel: 'team-zonda-dev',
     deployChannel: 'wg-zonda-deploy',
-    owner: 'codelittinc'
-  },
-  'ay-design-library': {
-    devGroup: '@ay-devs',
-    channel: 'team-ay-dev',
-    owner: 'codelittinc'
+    owner: 'codelittinc',
+    servers: ['dev-zonda', 'qa-zonda', 'prod-zonda']
   },
   'ay-design-library': {
     devGroup: '@ay-devs',
